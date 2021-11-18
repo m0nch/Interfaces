aeb418ef81e0ecf207658837c3afcf4cd83c9aeb
Margarita <Margarita@DATA_SERVER>
2021-11-18 13:55:52 +0400

    Changes

--- test_D/Program.cs
+++ test_D/Program.cs
@@ -13,16 +13,22 @@ namespace test_D
             Generate generate = new Generate(new Email(), new Sms(), new Mms());  
             List<IMessenger> message = generate.GetMessages();
 
-            //Email email = new Email();
-            //Sms sms = new Sms();
-            //Mms mms = new Mms();
-            //message.Add(email);
-            //message.Add(sms);
-            //message.Add(mms);
 
             for (int i = 0; i < message.Count; i++)
             {
                 Notification notification = new Notification(message[i]);
+                if ((IMessenger)message[i] is Email)
+                {
+                    notification.Text = "Notification: You have a new e-mail";
+                }
+                else if ((IMessenger)message[i] is Sms)
+                {
+                    notification.Text = "Notification: You have a new sms";
+                }
+                else if ((IMessenger)message[i] is Mms)
+                {
+                    notification.Text = "Notification: You have a new mms";
+                }
                 notification.DoNotify();
             }
 
@@ -32,37 +38,42 @@ namespace test_D
 
     public interface IMessenger
     {
-        void SendMessage();
+        void SendMessage(string text);
     }
 
     public class Generate
     {
-        Random rnd = new Random();
+        //Random rnd = new Random();
         List<IMessenger> message = new List<IMessenger>();
 
         public Generate(params IMessenger[] messenger)
-        {            
+        {
+            int maxCount = 20;
             for (int i = 0; i < messenger.Length; i++)
             {
                 message.Add(messenger[i]);
             }
-            while (message.Count < 6)
+            for (int i = messenger.Length; i < maxCount - messenger.Length; i+=3)
             {
-                message.Add(GenerateMessage());
+                message.Add(GenerateEmail());
+                message.Add(GenerateSms());
+                message.Add(GenerateMms());
             }
         }
 
-        public IMessenger GenerateMessage()
+        public IMessenger GenerateEmail()
         {
-            int r = rnd.Next(1, 5);
+            return new Email();
+        }
 
-            switch (r)
-            {
-                case 1: return new Email();
-                case 2: return new Sms();
-                case 3: return new Mms();
-            }
+        public IMessenger GenerateSms()
+        {
             return new Sms();
+        }
+
+        public IMessenger GenerateMms()
+        {
+            return new Mms();
         }
 
         public List<IMessenger> GetMessages()
@@ -72,38 +83,39 @@ namespace test_D
     }
     public class Email : IMessenger
     {
-        public void SendMessage()
+        public void SendMessage(string text)
         {
-            Console.WriteLine("Send e-mail");        
+            Console.WriteLine(text);        
         }
     }
 
     public class Sms : IMessenger
     {
-        public void SendMessage()
+        public void SendMessage(string text)
         {
-            Console.WriteLine("Send sms"); ;
+            Console.WriteLine(text); ;
         }
     }
 
     public class Mms : IMessenger
     {
-        public void SendMessage()
+        public void SendMessage(string text)
         {
-            Console.WriteLine("Send mms");        
+            Console.WriteLine(text);        
         }
     }
 
     public class Notification
     {
         private IMessenger _iMessenger;
+        public string Text { get; set; }
         public Notification(IMessenger iMessenger)
         {
             _iMessenger = iMessenger;
         }
         public void DoNotify()
         {
-            _iMessenger.SendMessage();
+            _iMessenger.SendMessage(Text);
         }
     }
 }
